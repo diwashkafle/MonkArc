9dbcb816357e0361b1cf58ced708223adcbc96f4
Mr.Codi <diwashhkafle66@gmail.com>
2025-11-29 19:54:20 +0545

    initial phase of authentication with google and github is completed

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,9 +3,15 @@ import Google from 'next-auth/providers/google'
 import GitHub from 'next-auth/providers/github'
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import { db } from '@/db'
+import { accounts, sessions, users, verificationTokens } from '@/db/schema'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: DrizzleAdapter(db),
+  adapter: DrizzleAdapter(db,{
+     usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
