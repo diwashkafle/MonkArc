dcde13540735c52c736f332006b765522544120e
Mr.Codi <diwashhkafle66@gmail.com>
2025-12-21 16:37:22 +0545

    securing the neon connection URI

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,6 @@
-import Link from 'next/link'
-import { getPublicJourneysForFeed, getPublicStats } from '@/lib/queries/journey-queries'
-import { db } from '@/db'
-import { users } from '@/db/schema'
-import { eq } from 'drizzle-orm'
-import { auth } from '@/lib/auth'
 import LandingPage from '@/components/PublicComponents/LandingPage'
 
 export default async function HomePage() {
-  const publicJourneys = await getPublicJourneysForFeed(12)
-  const stats = await getPublicStats()
-  const session = await auth();
   
   return (
     <LandingPage/>
